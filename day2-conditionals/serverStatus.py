c8df1e305f4af7563eac09f22eb36fb89dc369f1
Josias Mendes <josiaswinder@gmail.com>
2026-05-31 21:27:27 +0100

    Server Status Updated

--- day2-conditionals/serverStatus.py
+++ day2-conditionals/serverStatus.py
@@ -5,8 +5,8 @@ ram = float(input("Digite o valor da RAM: "))
 if cpu > 80:
     print("CPU Crítica")
 
-elif ram > 80:
+if ram > 80:
     print("RAM Crítica")
 
-else:
-    print("Servidor saudável")
+if cpu <= 80 and ram <= 80:
+    print("SERVIDOR SAUDÁVEL")
